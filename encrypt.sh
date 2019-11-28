70514b3260673080daf040abe56d059f4a97d0c0
Slenderchat <steeplike@gmail.com>
2019-11-28 05:06:58 +0300

    Tweaked encrypt.sh and reworked keyring once again, encrypte with new gpg key

--- encrypt.sh
+++ encrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in crypt/*
 do
-	gpg --batch --yes -o "encrypted/$(basename $file).gpg" -r steeplike@gmail.com -e $file &&
+	gpg --batch --yes -o "encrypted/$(basename $file).gpg" -r steeplike@gmail.com -se $file &&
 	mv -f $file -t decrypted
 done
